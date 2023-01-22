1764edc11264a129c07e42279cef5e37f5ca5dfd
aliamnieh <117279015+aliamnieh@users.noreply.github.com>
2023-01-22 04:47:21 -0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //برنامه که فاصله ی دو نقطه در صفحه را از یکدیگر محاسبه کند
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "")
+            {
+                Point a = new Point();
+                Point b = new Point();
+                a.X = Convert.ToDouble(textBox1.Text);
+                a.Y = Convert.ToDouble(textBox2.Text);
+                b.X = Convert.ToDouble(textBox3.Text);
+                b.Y = Convert.ToDouble(textBox4.Text);
+                textBox5.Text = Convert.ToString(Point.calculatedistance(a.X, a.Y, b.X, b.Y));
+            }
+            else MessageBox.Show("fill all the textboxes!");
+        }
+    }
+}
---
+++ Point.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace form4
+{
+    class Point
+    {
+        private double x;
+        private double y;
+        public double X
+        {
+            set
+            {
+                x = value;
+            }
+            get
+            {
+                return x;
+            }
+        }
+        public double Y
+        {
+            set
+            {
+                y = value;
+            }
+            get
+            {
+                return y;
+            }
+        }
+        public static double calculatedistance(double x1,double y1,double x2,double y2)
+        {
+            double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            return distance;
+        }
+    }
+}
